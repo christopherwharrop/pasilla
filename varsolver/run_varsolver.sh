43df54717edec1fe4c0dd9a4155b2942fe5347e2
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-08-19 21:51:17 +0000

    Update run_varsolver.sh to handle new location of executable.

--- varsolver/run_varsolver.sh
+++ varsolver/run_varsolver.sh
@@ -5,7 +5,7 @@ module load intel
 GPTL_PATH=/contrib/gptl/gptl-v5.5_nompi_noomp/bin
 PARSE_PATH=/home/Christopher.W.Harrop/bin
 
-./varsolver.exe > varsolver.stdout
+./exe/varsolver.exe > varsolver.stdout
 
 ${GPTL_PATH}/hex2name.pl ./varsolver.exe ./timing.0 > ./timing.varsolver.txt
 

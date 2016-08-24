73161ccab3822762b012626b11ff7b8e6baf540d
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-08-24 20:17:01 +0000

    Update plot script to display forcing and delta_t in title.

--- models/lorenz96/bin/plot_lorenz96.rb
+++ models/lorenz96/bin/plot_lorenz96.rb
@@ -3,12 +3,27 @@
 input_file=ARGV[0]
 output_file=input_file.sub(/\..*$/,'.png')
 
+# Get model forcing and delta_t
+forcing=0.0
+delta_t=0.0
+f = File.new(input_file)
+f.each do |line|
+  break if line=~/^\s*$/
+  if line=~/^model_forcing,\s*(\S+)$/
+    forcing=$1
+  end
+  if line=~/^model_delta_t,\s*(\S+)$/
+    delta_t=$1
+  end
+end
+f.close
+
 # Create a plot for each reservation
 IO.popen("gnuplot","w") do |gnuplot|
   gnuplot.puts "set datafile separator ','"
   gnuplot.puts "set xlabel('X')"
   gnuplot.puts "set ylabel('State(x)')"
-  gnuplot.puts "set title 'Lorenz96 Model State'"
+  gnuplot.puts "set title 'Lorenz96 Model State (forcing=#{forcing}, delta_t=#{delta_t})'"
   gnuplot.puts "set output '#{output_file}'"
   gnuplot.puts "set terminal png size 1000,600"
   gnuplot.puts "plot '#{input_file}' index 1 using 1:3 with linespoints"

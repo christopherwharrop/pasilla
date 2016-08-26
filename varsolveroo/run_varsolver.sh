78af9f8e68af4d521df66ec6853002dc2ee9e424
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-08-26 15:54:03 +0000

    Add copy of varsolver for OO development.

---
+++ varsolveroo/run_varsolver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+module load intel
+
+GPTL_PATH=/contrib/gptl/gptl-v5.5_nompi_noomp/bin
+PARSE_PATH=/home/Christopher.W.Harrop/bin
+
+./exe/varsolver.exe > varsolver.stdout
+
+${GPTL_PATH}/hex2name.pl ./varsolver.exe ./timing.0 > ./timing.varsolver.txt
+
+${PARSE_PATH}/parsetiming.rb -t 1 -d 6 timing.varsolver.txt > timing.varsolver.parse.txt

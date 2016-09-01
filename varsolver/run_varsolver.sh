2c2d0fbfff00ab4cb6683661e23252a022f43ea2
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-09-01 20:59:43 +0000

    Fix path to varsolver.exe in run script so that hex2name.pl can function.

--- varsolver/run_varsolver.sh
+++ varsolver/run_varsolver.sh
@@ -7,6 +7,6 @@ PARSE_PATH=/home/Christopher.W.Harrop/bin
 
 ./exe/varsolver.exe > varsolver.stdout
 
-${GPTL_PATH}/hex2name.pl ./varsolver.exe ./timing.0 > ./timing.varsolver.txt
+${GPTL_PATH}/hex2name.pl ./exe/varsolver.exe ./timing.0 > ./timing.varsolver.txt
 
 ${PARSE_PATH}/parsetiming.rb -t 1 -d 6 timing.varsolver.txt > timing.varsolver.parse.txt

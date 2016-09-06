c8a0a6bbabc0f47c1a2b7ee3e99bd395faa17517
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-09-06 15:15:32 +0000

    Add module for storing mathematical and physical constants.

---
+++ utilities/src/module_constants.f90
@@ -0,0 +1,13 @@
+module module_constants
+
+  use module_kind, only : r8kind
+
+  implicit none
+  private 
+
+  public :: PI
+
+  real(r8kind), parameter :: PI = 3.14159265359
+
+end module module_constants
+

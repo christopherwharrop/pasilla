81f8a36833a4858044aaedde978fed612c7a0661
Christopher Harrop <christopher.w.harrop@noaa.gov>
2016-10-21 21:48:58 +0000

    Add placeholder for making larger sets of observations for Lorenz96.

--- models/lorenz96/src/harvest_lorenz96_obs.f90
+++ models/lorenz96/src/harvest_lorenz96_obs.f90
@@ -11,16 +11,29 @@ program lorenz96Model
   implicit none
 
   integer :: step, i, mthd           ! Loop index
-  integer :: obs_idx(15)             ! Array of observation indices
-  integer :: obs_tim(15)             ! Array of observation times
+!  integer :: obs_idx(15)             ! Array of observation indices
+!  integer :: obs_tim(15)             ! Array of observation times
+  integer :: obs_idx(1000)             ! Array of observation indices
+  integer :: obs_tim(1000)             ! Array of observation times
   type(lorenz96_type) :: obs_data(3) ! Array of the observation data
   integer :: ierr                    ! Error code 
   character(len=128) :: filename
   integer :: fileunit
 
+  integer :: j, k
+
   ! Set the indices of the obs locations
-  obs_idx=(/4, 6, 8, 11, 12, 14, 18, 19, 25, 27, 29, 30, 33, 35, 40/)
-  obs_tim=(/1, 2, 3,  2,  1,  2,  3,  2,  1,  2,  3,  2,  1,  2,  3/)
+!  obs_idx=(/4, 6, 8, 11, 12, 14, 18, 19, 25, 27, 29, 30, 33, 35, 40/)
+!  obs_tim=(/1, 2, 3,  2,  1,  2,  3,  2,  1,  2,  3,  2,  1,  2,  3/)
+
+  j=1
+  do i = 1, 4000, 4
+    obs_idx(j) = i
+    k=modulo(j, 4)
+    if (k ==0 ) k = 2
+    obs_tim(j) = k
+    j = j + 1
+  end do
 
   ! Loop over experiment analysis times
   do step = 40060, 80000, 60
